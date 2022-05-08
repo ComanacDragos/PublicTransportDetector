8c538245f9ecccc619d451b08dd10477c23f9e61
WildUrti <46956225+WildUrti@users.noreply.github.com>
2022-05-08 15:11:45 +0300

    model v43 cutout=256

--- object_detection/mean_average_precision_visualizer.py
+++ object_detection/mean_average_precision_visualizer.py
@@ -146,6 +146,7 @@ if __name__ == '__main__':
         'model_v40': "cutout=32",
         'model_v41': "cutout=128",
         'model_v42': "cutout=192",
+        'model_v43': "cutout=256",
     }
 
     visualize(0.5, 0.3, None,
